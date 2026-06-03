fa31ae96fc21c6ccb97428102923af646e7f6105
Gabrax <gab-oz@wp.pl>
2026-06-03 11:40:30 +0200

    udp

--- src/main.c
+++ src/main.c
@@ -1,3 +1,14 @@
+#pragma comment(lib, "user32.lib")
+#pragma comment(lib, "d3d11.lib")
+#pragma comment(lib, "dxgi.lib")
+#pragma comment(lib, "Ws2_32.lib")
+
+#pragma comment(lib, "mfplat.lib")
+#pragma comment(lib, "mf.lib")
+#pragma comment(lib, "mfreadwrite.lib")
+#pragma comment(lib, "mfuuid.lib")
+#pragma comment(lib, "ole32.lib")
+
 #define WIN32_LEAN_AND_MEAN
 
 #include <windows.h>
@@ -16,13 +27,24 @@
 #include <mfidl.h>
 #include <mfobjects.h>
 #include <mftransform.h>
+#include <mfobjects.h>
+#include <mfreadwrite.h>
 
 #include <codecapi.h>
+
+#include <wmcodecdsp.h>
 
 static ID3D11Device* g_Device = NULL;
 static ID3D11DeviceContext* g_Context = NULL;
 static IDXGISwapChain* g_SwapChain = NULL;
 static ID3D11RenderTargetView* g_RTV = NULL;
+
+#define PORT 7777
+
+static SOCKET s_Socket = INVALID_SOCKET;
+
+static struct sockaddr_in s_RemoteAddr;
+static int s_RemoteAddrLen = sizeof(s_RemoteAddr);
 
 static LRESULT CALLBACK WindowProc(
     HWND hwnd,
@@ -33,7 +55,14 @@ static LRESULT CALLBACK WindowProc(
     switch (msg)
     {
         case WM_DESTROY:
-        {
+        {            
+            if (s_Socket != INVALID_SOCKET)
+            {
+                closesocket(s_Socket);
+                s_Socket = INVALID_SOCKET;
+            }
+
+            WSACleanup();
             PostQuitMessage(0);
             return 0;
         }
@@ -56,15 +85,10 @@ static int InitD3D11(HWND hwnd)
     scd.BufferDesc.Width = 1280;
     scd.BufferDesc.Height = 720;
     scd.BufferDesc.Format = DXGI_FORMAT_R8G8B8A8_UNORM;
-
     scd.BufferUsage = DXGI_USAGE_RENDER_TARGET_OUTPUT;
-
     scd.OutputWindow = hwnd;
-
     scd.SampleDesc.Count = 1;
-
     scd.Windowed = TRUE;
-
     scd.SwapEffect = DXGI_SWAP_EFFECT_DISCARD;
 
     HRESULT hr = D3D11CreateDeviceAndSwapChain(
@@ -145,6 +169,131 @@ static void Render(void)
         0);
 }
 
+static int HostConnection(void)
+{
+    WSADATA wsa;
+
+    if (WSAStartup(MAKEWORD(2,2), &wsa))
+        return 0;
+
+    s_Socket = socket(AF_INET, SOCK_DGRAM, IPPROTO_UDP);
+
+    if (s_Socket == INVALID_SOCKET)
+        return 0;
+
+    struct sockaddr_in addr;
+
+    ZeroMemory(&addr, sizeof(addr));
+
+    addr.sin_family = AF_INET;
+    addr.sin_port = htons(PORT);
+    addr.sin_addr.s_addr = INADDR_ANY;
+
+    if (bind(
+        s_Socket,
+        (SOCKADDR*)&addr,
+        sizeof(addr)) == SOCKET_ERROR)
+    {
+        return 0;
+    }
+
+    printf("Waiting for client...\n");
+
+    char buffer[64];
+
+    int received = recvfrom(
+        s_Socket,
+        buffer,
+        sizeof(buffer),
+        0,
+        (SOCKADDR*)&s_RemoteAddr,
+        &s_RemoteAddrLen);
+
+    if (received <= 0) return 0;
+
+    if (received >= sizeof(buffer))
+        received = sizeof(buffer) - 1;
+
+    buffer[received] = 0;
+
+    printf("Client connected: %s\n",inet_ntoa(s_RemoteAddr.sin_addr));
+
+    sendto(
+        s_Socket,
+        "OK",
+        2,
+        0,
+        (SOCKADDR*)&s_RemoteAddr,
+        s_RemoteAddrLen);
+
+    return 1;
+}
+
+static int JoinConnection(void)
+{
+    WSADATA wsa;
+
+    if (WSAStartup(MAKEWORD(2,2), &wsa))
+        return 0;
+
+    s_Socket = socket(
+        AF_INET,
+        SOCK_DGRAM,
+        IPPROTO_UDP);
+
+    if (s_Socket == INVALID_SOCKET)
+        return 0;
+
+    char ip[64];
+
+    printf("IP: ");
+    scanf("%63s", ip);
+
+    ZeroMemory(&s_RemoteAddr, sizeof(s_RemoteAddr));
+
+    s_RemoteAddr.sin_family = AF_INET;
+    s_RemoteAddr.sin_port = htons(7777);
+
+    inet_pton(
+        AF_INET,
+        ip,
+        &s_RemoteAddr.sin_addr);
+
+    sendto(
+        s_Socket,
+        "HELLO",
+        5,
+        0,
+        (SOCKADDR*)&s_RemoteAddr,
+        sizeof(s_RemoteAddr));
+
+    char buffer[64];
+
+    int received = recvfrom(
+        s_Socket,
+        buffer,
+        sizeof(buffer),
+        0,
+        NULL,
+        NULL);
+
+    if (received <= 0)
+        return 0;
+
+    return 1;
+}
+
+void SendFrame(void* data, int size)
+{
+    sendto(
+        s_Socket,
+        (const char*)data,
+        size,
+        0,
+        (SOCKADDR*)&s_RemoteAddr,
+        s_RemoteAddrLen);
+}
+
 int main(void)
 {
     HINSTANCE hInstance = GetModuleHandleA(NULL);
@@ -159,6 +308,33 @@ int main(void)
 
     printf("Console attached\n");
 
+    printf("1. Host\n");
+    printf("2. Join\n");
+
+    int mode = 0;
+
+    scanf_s("%d", &mode);
+
+    int connected = 0;
+
+    if (mode == 1) connected = HostConnection();
+    else if (mode == 2) connected = JoinConnection();
+
+    if (!connected)
+    {
+        printf("Connection failed\n");
+        return 0;
+    }
+
+    printf("Connection established\n");
+
+    u_long nonBlocking = 1;
+
+    ioctlsocket(
+        s_Socket,
+        FIONBIO,
+        &nonBlocking);
+
     WNDCLASSA wc;
 
     ZeroMemory(&wc, sizeof(wc));
@@ -170,7 +346,7 @@ int main(void)
     if (!RegisterClassA(&wc))
     {
         printf("RegisterClassA failed\n");
-        return -1;
+        return 0;
     }
 
     HWND hwnd = CreateWindowExA(
@@ -190,7 +366,7 @@ int main(void)
     if (!hwnd)
     {
         printf("CreateWindowExA failed\n");
-        return -1;
+        return 0;
     }
 
     ShowWindow(hwnd, SW_SHOW);
@@ -199,7 +375,7 @@ int main(void)
     if (!InitD3D11(hwnd))
     {
         printf("InitD3D11 failed\n");
-        return -1;
+        return 0;
     }
 
     MSG msg;
@@ -219,20 +395,39 @@ int main(void)
             DispatchMessageA(&msg);
         }
 
+        char packet[65536];
+
+        int received = recvfrom(
+            s_Socket,
+            packet,
+            sizeof(packet),
+            0,
+            NULL,
+            NULL);
+
+        if (received > 0)
+        {
+            printf("Received %d bytes\n", received);
+        }
+
         Render();
     }
 
-    if (g_RTV)
-        g_RTV->lpVtbl->Release(g_RTV);
+    if (g_RTV) g_RTV->lpVtbl->Release(g_RTV);
 
-    if (g_SwapChain)
-        g_SwapChain->lpVtbl->Release(g_SwapChain);
+    if (g_SwapChain) g_SwapChain->lpVtbl->Release(g_SwapChain);
 
-    if (g_Context)
-        g_Context->lpVtbl->Release(g_Context);
+    if (g_Context) g_Context->lpVtbl->Release(g_Context);
 
-    if (g_Device)
-        g_Device->lpVtbl->Release(g_Device);
+    if (g_Device) g_Device->lpVtbl->Release(g_Device);
+
+    if (s_Socket != INVALID_SOCKET)
+    {
+        closesocket(s_Socket);
+        s_Socket = INVALID_SOCKET;
+    }
+
+    WSACleanup();
 
     return 0;
 }
